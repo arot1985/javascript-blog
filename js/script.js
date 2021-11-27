697b02819cd53e7004766b42078f2d78981329f5
Anna Rotkiel-Kozik <anna.rotkiel@wp.pl>
2021-11-27 16:12:26 +0100

    Add tag list

--- js/script.js
+++ js/script.js
@@ -45,6 +45,9 @@
     optTitleListSelector = '.titles',
     optArticleTagsSelector = '.post-tags .list';
     optArticleAuthorsSelector = '.post-author';
+    optTagsListSelector = '.tags .list';
+    optCloudClassCount = 5;
+    optCloudClassPrefix = 'tag-size-';
 
     function generateTitleLinks(customSelector = ''){
 
@@ -91,7 +94,38 @@
 
   generateTitleLinks();
 
+  function calculateTagsParams (tags){
+    const params = {
+      max: 0,
+      min: 999999,
+    }
+
+    for(let tag in tags){
+      if(tags[tag] > params.max){
+        params.max = tags[tag];
+      } 
+      if(tags[tag] < params.min) {
+        params.min = tags[tag];
+      }
+      console.log(tag + ' is used ' + tags[tag] + ' times');
+    }
+
+    return params;
+  }
+
+  function calculateTagClass(count, params){
+    const normalizedCount = count - params.min;
+    const normalizedMax = params.max - params.min;
+    const percentage = normalizedCount / normalizedMax;
+    const classNumber = Math.floor( percentage * (optCloudClassCount - 1) + 1 );
+    return optCloudClassPrefix + classNumber;
+
+  }
+
   function generateTags(){
+     /* [NEW] create a new variable allTags with an empty object */
+  let allTags = {};
+
     /* find all articles */
     const articles = document.querySelectorAll(optArticleSelector);
 
@@ -123,14 +157,47 @@
         /* add generated code to html variable */
         html = html + ' ' + tagLinkHTML;
 
+        /* [NEW] check if this link is NOT already in allTags */
+        if(!allTags[tag]) {
+        /* [NEW] add tag to allTags object */
+        allTags[tag] = 1;
+        } else {
+        allTags[tag]++;
+      }
+
       /* END LOOP: for each tag */
       }
+
       /* insert HTML of all the links into the tags wrapper */
       tagsList.innerHTML = html;
 
     /* END LOOP: for every article: */
     }
+    /* [NEW] find list of tags in right column */
+    const tagList = document.querySelector('.tags');
+
+    /* [NEW] create variable for all links HTML code */
+    const tagsParams = calculateTagsParams(allTags);
+    console.log('tagsParams:', tagsParams)
+    let allTagsHTML = '';
+
+    /* [NEW] START LOOP: for each tag in allTags: */
+      for(let tag in allTags){
+
+      /* [NEW] generate code of a link and add it to allTagsHTML */
+      const tagLinkHTML ='<li><a href="#tag-' + tag + '" class= "' + calculateTagClass(allTags[tag], tagsParams) + '">' + tag + '</a></li>';
+      console.log('tagLinkHTML:', tagLinkHTML);
+
+     allTagsHTML += tagLinkHTML;
+     console.log(allTagsHTML);
+    
+      /* [NEW] END LOOP: for each tag in allTags: */
+      }
+  
+    /*[NEW] add HTML from allTagsHTML to tagList */
+    tagList.innerHTML = allTagsHTML;
   }
+
 
   generateTags();
 
@@ -181,7 +248,8 @@
 
   function addClickListenersToTags(){
     /* find all links to tags */
-    const allLinksToTags = document.querySelectorAll(".post-tags a")
+    const allLinksToTags = document.querySelectorAll('.tags a, .post-tags a');
+    console.log(allLinksToTags);
 
     /* START LOOP: for each link */
     for(let allLinkToTags of allLinksToTags){
